653d71adb356bb815e25424e016df748513ef8b8
unknown <nkosimova06@gmail.com>
2020-04-22 01:07:09 +0500

    create project, add data access

---
+++ DataAccess/MSSQL.cs
@@ -0,0 +1,7 @@
+namespace CR_DataAccess
+{
+    static public class MSSqlDA
+    {
+        public const string ConnectionString = @"Data source=KosimovaNodira; Initial catalog = Alif_Academy; Integrated Security=True";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using CR_DataAccess;
+namespace CreditRequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
